045937fe40b92085c7db187064e483b0c6a200df
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-05-22 17:58:02 +0200

    feat: add source

--- video_segmenter.c
+++ video_segmenter.c
@@ -18,6 +18,8 @@ typedef enum {
     SEG_ERR = -1
 } SegResult;
 
+// @see https://github.com/catchorg/Catch2/issues/929#issuecomment-308663820
+// @see https://github.com/catchorg/Catch2/issues/929#issuecomment-308663820
 #define CHECK(cond, msg, ...)                                     \
 do {                                                          \
     if (cond) {                                               \
